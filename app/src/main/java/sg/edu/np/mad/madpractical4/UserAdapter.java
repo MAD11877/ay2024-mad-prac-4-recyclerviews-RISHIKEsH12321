de1fba7f83361a5e1c47fb135eeee73756b3a152
Rishikesh <148840508+RISHIKEsH12321@users.noreply.github.com>
2024-05-06 15:34:09 +0800

    Submit Practical 4 Test 3

--- app/src/main/java/sg/edu/np/mad/madpractical4/UserAdapter.java
+++ app/src/main/java/sg/edu/np/mad/madpractical4/UserAdapter.java
@@ -1,46 +1,60 @@
 package sg.edu.np.mad.madpractical4;
 
-import static androidx.core.content.ContextCompat.startActivity;
-
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class UserAdapter extends RecyclerView.Adapter<UserViewHolder>{
-    private ArrayList<User> list_objects;
-    private static final String TAG = "list_objects";
+public class UserAdapter extends RecyclerView.Adapter<UserViewHolder> {
+    ArrayList<User> data;
 
-    public UserAdapter(ArrayList<User> list_objects, ListActivity activity){
-        this.list_objects = list_objects;
+    private static final String TAG = "UserAdapter";
+
+    public UserAdapter(ArrayList<User> input) {
+        data = input;
     }
 
-    public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_activity_list, parent, false);
-        UserViewHolder holder = new UserViewHolder(view);
-        return holder;
+    public UserViewHolder onCreateViewHolder(
+            ViewGroup parent,
+            int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.custom_activity_list,
+                parent,
+                false);
+        return new UserViewHolder(item);
     }
 
-    public void onBindViewHolder(UserViewHolder holder, int position){
-        User list_items = list_objects.get(position);
-        holder.name.setText(list_items.getName());
-        holder.description.setText(list_items.getDescription());
-        Log.d(TAG, list_items.getName());
+    public void onBindViewHolder(
+            UserViewHolder holder,
+            int position) {
+        User s = data.get(position);
+        holder.name.setText(s.name);
+        holder.des.setText(s.description);
 
-        int lastDigit = Integer.parseInt(list_items.getName()) % 10;
-        if (lastDigit == 7){
-            holder.bigImage.setVisibility(View.VISIBLE);
+        Log.d(TAG, "onBindViewHolder Name: " + s.name);
+        char Lastchar = s.name.charAt(s.name.length() - 1);
+        if (Lastchar == '7'){
+            holder.bigImg.setVisibility(View.VISIBLE);
+            Log.d(TAG, "onBindViewHolder: Visible" );
         }
         else{
-            holder.bigImage.setVisibility(View.GONE);
+            holder.bigImg.setVisibility(View.GONE);
         }
     }
 
-    public int getItemCount(){return list_objects.size();}
-
-    // Number 7 Here
-
+    public int getItemCount() {
+        return data.size();
+    }
 }
+
+
