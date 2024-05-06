9885081c7e069a3b9b3af911b20b0c6a775340d0
Rishikesh <148840508+RISHIKEsH12321@users.noreply.github.com>
2024-05-06 17:36:54 +0800

    Submit Practical 4 Test 4

--- app/src/main/java/sg/edu/np/mad/madpractical4/ListActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical4/ListActivity.java
@@ -1,24 +1,30 @@
 package sg.edu.np.mad.madpractical4;
 
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
-import java.util.ArrayList;
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import java.util.Random;
+import android.util.Log;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+import java.io.*;
+import java.util.Collections;
 import java.util.Random;
 
+import android.view.View;
+import android.widget.ImageView;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -33,43 +39,61 @@ public class ListActivity extends AppCompatActivity {
             return insets;
         });
 
-        //Creating 20 Users
-        ArrayList<User> userList = new ArrayList<User>();
-        Random random = new Random();
-        userList.add(new User("Alice" + new Random().nextInt(99999), "Software Engineer", 1, false));
-        userList.add(new User("Bob" + new Random().nextInt(99999), "Data Scientist", 2, true));
-        userList.add(new User("Charlie" + new Random().nextInt(99999), "UI/UX Designer", 3, false));
-        userList.add(new User("David" + new Random().nextInt(99999), "Product Manager", 4, true));
-        userList.add(new User("Emma" + new Random().nextInt(99999), "Frontend Developer", 5, false));
-        userList.add(new User("Frank" + new Random().nextInt(99999), "Backend Developer", 6, true));
-        userList.add(new User("Grace" + new Random().nextInt(99999), "Mobile App Developer", 7, false));
-        userList.add(new User("Henry" + new Random().nextInt(99999), "DevOps Engineer", 8, true));
-        userList.add(new User("Ivy" + new Random().nextInt(99999), "Systems Analyst", 9, false));
-        userList.add(new User("Jack" + new Random().nextInt(99999), "Database Administrator", 10, true));
-        userList.add(new User("Kevin" + new Random().nextInt(99999), "Network Engineer", 11, false));
-        userList.add(new User("Lily" + new Random().nextInt(99999), "Cybersecurity Analyst", 12, true));
-        userList.add(new User("Mike" + new Random().nextInt(99999), "Cloud Architect", 13, false));
-        userList.add(new User("Nancy" + new Random().nextInt(99999), "AI Specialist", 14, true));
-        userList.add(new User("Oliver" + new Random().nextInt(99999), "Game Developer", 15, false));
-        userList.add(new User("Pam" + new Random().nextInt(99999), "Embedded Systems Engineer", 16, true));
-        userList.add(new User("Quinn" + new Random().nextInt(99999), "Web Developer", 17, false));
-        userList.add(new User("Rachel" + new Random().nextInt(99999), "Software Tester", 18, true));
-        userList.add(new User("Sam" + new Random().nextInt(99999), "Data Engineer", 19, false));
-        userList.add(new User("Tom" + new Random().nextInt(99999), "Machine Learning Engineer", 20, true));
+//        Intent mainActivity = new Intent(ListActivity.this, MainActivity.class);
+//        int randomInt = new Random().nextInt(999999);
+//        Log.d("TAG", "My variable value is: " + randomInt);
+//        mainActivity.putExtra("randomInt", randomInt);
+//
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle("Profile");
+//        builder.setMessage("MADness");
+//        builder.setCancelable(false);
+//        builder.setPositiveButton("VIEW", new
+//                DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        startActivity(mainActivity);
+//                    }
+//                });
+
+        // Practical 4 Starts Here
+        ArrayList<User> myUser_List = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int name = new Random().nextInt(999999999);
+            int description = new Random().nextInt(999999999);
+            boolean followed = new Random().nextBoolean();
+
+            User user = new User("John Doe", "MAD Developer", 1, false);
+            user.setName("Name" + String.valueOf(name));
+            user.setFollowed(followed);
+            myUser_List.add(user);
+        }
+
+        // Add This (RecyclerView)
+        UserAdapter userAdapter = new UserAdapter(myUser_List, this);
+        RecyclerView recyclerView = findViewById(R.id.recycler_View);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(userAdapter);
 
 
-        RecyclerView rcView = findViewById(R.id.rvView);
-        UserAdapter mAdapter = new UserAdapter(userList);
-
-        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
-
-        rcView.setLayoutManager(mLayoutManager);
-        rcView.setItemAnimator(new DefaultItemAnimator());
-        rcView.setAdapter(mAdapter);
-
-        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-
-
-
+//        builder.setNegativeButton("CLOSE", new
+//                DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                    }
+//                });
+//
+//        AlertDialog alertDialog = builder.create();
+//
+//        ImageView profileBtn = findViewById(R.id.foreground);
+//        profileBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                alertDialog.show();
+//            }
+//        });
     }
 }
--- app/src/main/java/sg/edu/np/mad/madpractical4/UserAdapter.java
+++ app/src/main/java/sg/edu/np/mad/madpractical4/UserAdapter.java
@@ -1,60 +1,46 @@
 package sg.edu.np.mad.madpractical4;
 
-import android.content.DialogInterface;
-import android.content.Intent;
+import static androidx.core.content.ContextCompat.startActivity;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class UserAdapter extends RecyclerView.Adapter<UserViewHolder> {
-    ArrayList<User> data;
+public class UserAdapter extends RecyclerView.Adapter<UserViewHolder>{
+    private ArrayList<User> list_objects;
+    private static final String TAG = "list_objects";
 
-    private static final String TAG = "UserAdapter";
-
-    public UserAdapter(ArrayList<User> input) {
-        data = input;
+    public UserAdapter(ArrayList<User> list_objects, ListActivity activity){
+        this.list_objects = list_objects;
     }
 
-    public UserViewHolder onCreateViewHolder(
-            ViewGroup parent,
-            int viewType) {
-        View item = LayoutInflater.from(parent.getContext()).inflate(
-                R.layout.custom_activity_list,
-                parent,
-                false);
-        return new UserViewHolder(item);
+    public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_activity_list, parent, false);
+        UserViewHolder holder = new UserViewHolder(view);
+        return holder;
     }
 
-    public void onBindViewHolder(
-            UserViewHolder holder,
-            int position) {
-        User s = data.get(position);
-        holder.name.setText(s.name);
-        holder.des.setText(s.description);
+    public void onBindViewHolder(UserViewHolder holder, int position){
+        User list_items = list_objects.get(position);
+        holder.name.setText(list_items.getName());
+        holder.description.setText(list_items.getDescription());
+        Log.d(TAG, list_items.getName());
 
-        Log.d(TAG, "onBindViewHolder Name: " + s.name);
-        char Lastchar = s.name.charAt(s.name.length() - 1);
-        if (Lastchar == '7'){
-            holder.bigImg.setVisibility(View.VISIBLE);
-            Log.d(TAG, "onBindViewHolder: Visible" );
+        int lastDigit = Integer.parseInt(list_items.getName()) % 10;
+        if (lastDigit == 7){
+            holder.bigImage.setVisibility(View.VISIBLE);
         }
         else{
-            holder.bigImg.setVisibility(View.GONE);
+            holder.bigImage.setVisibility(View.GONE);
         }
     }
 
-    public int getItemCount() {
-        return data.size();
-    }
+    public int getItemCount(){return list_objects.size();}
+
+    // Number 7 Here
+
 }
-
-
